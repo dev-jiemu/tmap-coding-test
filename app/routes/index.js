82b711c528314b133b8df0e3e796fedd49ea0939
jiemu <rebianne03@naver.com>
2025-07-09 14:39:57 +0900

    init

--- app/routes/index.js
+++ app/routes/index.js
@@ -7,4 +7,12 @@ router.get('/index', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.post('/api/upload-poi', (req, res, next) => {
+
+})
+
+router.get('/api/upload-poi', (req, res, next) => {
+
+})
+
 module.exports = router;
